fb9b4d041b028e5ee124f8c6dc89457143f5c1d9
Артем Жолудь <zholud.artem@gmail.com>
2022-05-15 21:43:49 +0300

    добавил сообщение с приглашением добавить первую точку маршрута

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,6 +1,8 @@
 import TaskListTemplateView from '../view/task-list-view.js';
 import TaskEventTemplateView from '../view/task-event-view.js';
 import TaskEditTemplateView from '../view/task-edit-view.js';
+import NoPointTemplateView from '../view/no-point-view.js';
+import ListSortTemplateView from '../view/list-sort-view';
 import {render} from '../render.js';
 
 export default class BoardPresenter {
@@ -11,17 +13,15 @@ export default class BoardPresenter {
   #pointEventComponent = new TaskEventTemplateView();
 
   #boardPoints = [];
-  init = (listContainer,pointsModel) => {
+
+  constructor(listContainer,pointsModel) {
     this.#listContainer = listContainer;
     this.#pointsModel = pointsModel;
+  }
 
+  init = () => {
     this.#boardPoints = [...this.#pointsModel.points];
-
-    render(this.#pointListComponent, this.#listContainer);
-
-    for (let i = 0; i < this.#boardPoints.length; i++) {
-      this.#renderPoint(this.#boardPoints[i]);
-    }
+    this.#renderBoard();
   };
 
   #renderPoint = (point) => {
@@ -60,5 +60,18 @@ export default class BoardPresenter {
     });
 
     render(pointComponent, this.#pointListComponent.element);
+  };
+
+  #renderBoard = () => {
+    if(this.#boardPoints.every((point) => point.isArchive)){
+      render(new NoPointTemplateView(), this.#listContainer);
+    } else {
+      render(new ListSortTemplateView(), this.#listContainer);
+      render(this.#pointListComponent, this.#listContainer);
+
+      for (let i = 0; i < this.#boardPoints.length; i++) {
+        this.#renderPoint(this.#boardPoints[i]);
+      }
+    }
   };
 }
---
+++ src/view/board-view.js
@@ -0,0 +1,22 @@
+import {createElement} from '../render.js';
+const createBoardTemplate = () => '<section class="trip-events"><h2 class="visually-hidden">Trip events</h2></section>';
+
+export default class BoardView {
+  #element = null;
+
+  get template() {
+    return createBoardTemplate();
+  }
+
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
+    }
+
+    return this.#element;
+  }
+
+  removeElement() {
+    this.#element = null;
+  }
+}
