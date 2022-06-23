bf6bf7675496f8ef3f2350cc469aaeacd5ccbc57
Artem Zholud <zholud.artem@gmail.com>
2022-06-23 09:12:39 +0200

    f

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -1,7 +1,7 @@
 import {generatePoints} from '../mock/points.js';
 
 export default class PointsModel {
-  #points = Array.from({length: 5}, generatePoints);
+  #points = Array.from({length: 5}, generatePoints());
 
   get points() {
     return this.#points;
